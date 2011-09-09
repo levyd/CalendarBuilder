fb96d8a51d66916a984cedb2bd1f1eb01a794f26
Alex Hart <alex.hart658@gmail.com>
2011-09-09 16:29:01 -0300

    Add wrapper function for flexibility
    This lets the code either be imported like:
    import parse
    parse.run()
    OR you can just run it on a command line like normal

--- parse.py
+++ parse.py
@@ -2,14 +2,17 @@
 import argparse
 from CalendarBuilder import CalendarBuilder
 
-argparser = argparse.ArgumentParser(
-    description = 'Convert a dalonline html calendar to iCal format'
-)
-argparser.add_argument('infile', type=argparse.FileType('r'))
-argparser.add_argument('outfile', type=argparse.FileType('w'))
-args = argparser.parse_args()
+def run():
+    argparser = argparse.ArgumentParser(
+        description = 'Convert a dalonline html calendar to iCal format'
+    )
+    argparser.add_argument('infile', type=argparse.FileType('r'))
+    argparser.add_argument('outfile', type=argparse.FileType('w'))
+    args = argparser.parse_args()
 
 
-webpage = CalendarBuilder() 
-webpage.feed(args.infile.read())
+    webpage = CalendarBuilder() 
+    webpage.feed(args.infile.read())
 
+if __name__ == '__main__':
+    run()
